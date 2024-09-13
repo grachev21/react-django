3d62a50ce2a2e29f54ef4f1e44e64be7acccfa94
grachev21 <grachev613@gmail.com>
2024-09-13 17:30:14 +0400

    first commit

---
+++ core/user/models.py
@@ -0,0 +1,9 @@
+import uuid
+
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin 
+from django.core.exceptions import ObjectDoesNotExist
+from django.db import models
+from django.http import Http404
+
+
+
